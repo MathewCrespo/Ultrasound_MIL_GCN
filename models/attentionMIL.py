ef8f13ac55e310dbf677f886c7241559ce74942a
liaohhhhhh <56508380+liaohhhhhh@users.noreply.github.com>
2021-02-23 19:39:43 +0800

    add clinical information concat

--- models/attentionMIL.py
+++ models/attentionMIL.py
@@ -429,3 +429,88 @@ class Res_Attention(nn.Module):
         neg_log_likelihood = -1. * (Y * torch.log(Y_prob) + (1. - Y) * torch.log(1. - Y_prob))  # negative log bernoulli
         #print("Y_prob is：", Y_prob)
         return neg_log_likelihood, Y_prob, A
+
+
+class C_Attention(nn.Module):
+    def __init__(self, input_dim=3, L=500, D=128, K=1, C=0):
+        super(C_Attention, self).__init__()
+        self.input_dim = input_dim
+        self.L = L
+        self.D = D
+        self.K = K
+        self.C = C
+
+        self.feature_extractor_part1 = nn.Sequential(
+            nn.Conv2d(self.input_dim, 20, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(20, 50, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2)
+            ##nn.AdaptiveMaxPool2d(4)
+        )
+
+        self.feature_extractor_part2 = nn.Sequential(
+            nn.Linear(50 * 53 * 53, self.L),
+            nn.ReLU(),
+        )
+
+        self.attention = nn.Sequential(
+            nn.Linear(self.L + self.C, self.D),
+            nn.Tanh(),
+            nn.Linear(self.D, self.K)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear((self.L+self.C) * self.K, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x, c, debug=False):
+        if debug:
+            print("input shape:", x.shape)
+        x = x.squeeze(0)
+        if debug:
+            print("squeeze shape:", x.shape)
+
+        H = self.feature_extractor_part1(x)
+        if debug:
+            print("feature_extractor_part1 shape:", H.shape)
+        H = H.view(-1, 50 * 53 * 53)
+        if debug:
+            print("view shape:", H.shape)
+        H = self.feature_extractor_part2(H)  # NxL
+        H = torch.cat([H, c.repeat(H.size(0), 1)], dim=1)
+        if debug:
+            print("feature_extractor_part2 shape:", H.shape)
+
+        A = self.attention(H)  # NxK
+        A = torch.transpose(A, 1, 0)  # KxN
+        A = F.softmax(A, dim=1)  # softmax over N
+
+        M = torch.mm(A, H)  # KxL
+        Y_prob = self.classifier(M)
+        Y_hat = torch.ge(Y_prob, 0.5).float()
+
+        return Y_prob, Y_hat, A
+
+    # AUXILIARY METHODS
+    def calculate_classification_error(self, X, C, Y):
+        Y = Y.float()
+        _, Y_hat, _ = self.forward(X, C)
+        error = 1. - Y_hat.eq(Y).cpu().float().mean().item()
+
+        return error, Y_hat
+
+    def calculate_objective(self, X, C, Y):
+        Y = Y.float()
+        Y_prob, _, A = self.forward(X, C)
+        Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
+        neg_log_likelihood = -1. * (Y * torch.log(Y_prob) + (1. - Y) * torch.log(1. - Y_prob))  # negative log bernoulli
+        #print("Y_prob is：", Y_prob)
+        return neg_log_likelihood, Y_prob, A
+
+    def calculate_weights(self, X):
+        Y_prob, Y_hat, weights = self.forward(X)
+        Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
+        return Y_prob, Y_hat, weights
